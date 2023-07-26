7b0f0bb0cffa59e74aa60ed08f730a6395d0020c
Kuborros <kubo@catbat.rocks>
2023-07-26 23:39:16 +0200

    Release 0.2.0, temp removal of unused patches for player code (will be backported from SpadeMod later)

---
+++ FP2Lib/Item/ItemIDBroker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FP2Lib.Item
+{
+    internal class ItemIDBroker
+    {
+        internal int lastItemID;
+
+    }
+}
---
+++ FP2Lib/Item/PotionIDBroker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FP2Lib.Item
+{
+    internal class PotionIDBroker
+    {
+        internal int lastPotionID;
+    }
+}
